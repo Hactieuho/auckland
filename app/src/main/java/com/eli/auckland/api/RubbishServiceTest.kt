7b9795703e337d9c2e9bd3e177f132a2607ff0de
Hactieuho96 <Hactieuho96@gmail.com>
2021-06-25 21:19:50 +0700

    - Test api

---
+++ app/src/main/java/com/eli/auckland/api/RubbishServiceTest.kt
@@ -0,0 +1,29 @@
+package com.eli.auckland.api
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL = "https://raw.githubusercontent.com/Hactieuho/auckland/main/app/src/main/assets/"
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface RubbishServiceTest {
+    @GET("/rubbish.json")
+    fun getRubbish(@Query("an") an: String?) : Call<Any?>?
+}
+
+object RubbishApiTest {
+    val api: RubbishServiceTest by lazy {
+        retrofit.create(RubbishServiceTest::class.java)
+    }
+}
